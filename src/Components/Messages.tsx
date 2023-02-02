b742e4d93610e90d5e715fa06987ca1cc77a7327
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2023-02-02 00:22:55 +0000

    Final Build

--- src/Components/Messages.tsx
+++ src/Components/Messages.tsx
@@ -1,14 +1,70 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import styles from '../Styles/messages.module.scss'
+import send_image from '../assets/send_button.png';
+import { useSelector } from 'react-redux';
+import {
+	doc,
+	onSnapshot,
+	getFirestore,
+	query,
+	collection,
+	deleteDoc,
+} from 'firebase/firestore';
+import delete_button from '../assets/delete_button.png'
+
 interface Props {
     firebase: any
 }
 
 const Messages: FC<Props> = ({firebase}) => {
+    const [messageslist, setMessagesList] = useState<Array<unknown>>([])
+  	const db = getFirestore(firebase);
+    const loginInfo = useSelector((state: any) => state.login);
+
+
+  const DeletePost = (id: string) => {
+    const confirmation = confirm('Do you really want to delete this message?')
+
+    if (confirmation) {
+        deleteDoc(doc(db, 'MSG', id)).then(() => {
+        })
+    }
+  }
+
+    useEffect(() => {
+
+      const q = query(collection(db, "MSG"))
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const MSG: Array<unknown> = [];
+				querySnapshot.forEach((doc) => {
+					MSG.push({...doc.data(), ID: doc.id});
+				});
+        MSG.sort((a: any, b: any) => a.order - b.order);
+        setMessagesList(MSG)
+      })
+    
+
+    }, [])
+    
 
   return (
-    <div className={styles.Messages}>Messages</div>
-  )
+		<div className={styles.Messages}>
+			{messageslist.map((el: any, index) => (
+				<li key={index}>
+					<div className={styles.TitleDate}>
+						<p className={styles.Name}>{el.user}</p>
+						<p className={styles.Date}>{el.date}</p>
+					</div>
+					<img className={styles.icon} src={el.pic} />
+					{el.email == loginInfo.Email && (
+						<img onClick={() => DeletePost(el.ID)} className={styles.delete} src={delete_button} />
+					)}
+					<p className={styles.messagebox}>{el.message}</p>
+				</li>
+			))}
+			<li></li>
+		</div>
+	);
 }
 
 export default Messages
--- src/Components/SendingFrame.tsx
+++ src/Components/SendingFrame.tsx
@@ -1,30 +1,75 @@
 import React, { Dispatch, FC, useState, SetStateAction } from 'react';
-import styles from '../Styles/sendingframe.module.scss'
+import styles from '../Styles/sendingframe.module.scss';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
 import { LoginAction } from '../assets/Redux/Actions/Actions';
+import send_image from '../assets/send_button.png';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 interface Props {
 	firebase: any;
 }
 
-const SendingFrame: FC<Props> = ({firebase}) => {
-    const [logged, setLogged] = useState<boolean>(false);
-  
-    return (
-			<div className={styles.sendingframe}>
-				<p>To send messages you need to login: </p>
-				<Login firebase={firebase} setLogged={setLogged} />
-			</div>
-		);
+const SendingFrame: FC<Props> = ({ firebase }) => {
+	const [logged, setLogged] = useState<boolean>(false);
+
+	return (
+		<div className={styles.sendingframe}>
+			{logged ? (
+				<SendingMessages firebase={firebase} />
+			) : (
+				<>
+					<p>To send messages you need to login 🔰: </p>
+					<Login firebase={firebase} setLogged={setLogged} />
+				</>
+			)}
+		</div>
+	);
+};
+
+interface MessageProps {
+	firebase: any;
 }
 
+const SendingMessages: FC<MessageProps> = ({ firebase }) => {
+	const [message, setMessage] = useState<string>('');
+	const db = getFirestore(firebase);
+	const loginInfo = useSelector((state: any) => state.login);
+
+	const UploadHandler = async () => {
+		setMessage('')
+		const today = new Date().toLocaleDateString();
+		const order = Date.now();
+		try {
+			const docRef = await addDoc(collection(db, 'MSG'), {
+				order: order,
+				user: loginInfo.Name,
+				pic: loginInfo.ProfilePic,
+				email: loginInfo.Email,
+				message: message,
+				date: today,
+			});
+		} catch (e) {
+		}
+	};
+
+	return (
+		<>
+			<input
+				className={styles.Input}
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+			/>
+			<img onClick={() => UploadHandler()} src={send_image} />
+		</>
+	);
+};
 
 interface LoginProps {
 	setLogged: Dispatch<SetStateAction<boolean>>;
-    firebase: any
+	firebase: any;
 }
-const Login: FC<LoginProps> = ({firebase, setLogged }) => {
+const Login: FC<LoginProps> = ({ firebase, setLogged }) => {
 	const dispatch = useDispatch();
 	const auth = getAuth(firebase);
 	const provider = new GoogleAuthProvider();
@@ -33,13 +78,11 @@ const Login: FC<LoginProps> = ({firebase, setLogged }) => {
 		signInWithPopup(auth, provider)
 			.then((result) => {
 				const user = result.user;
-                console.log(user)
 
 				if (user.displayName && user.photoURL && user.email) {
 					dispatch(LoginAction(user.displayName, user.photoURL, user.email));
 					setLogged(true);
 				} else {
-					console.log('super error');
 				}
 			})
 			.catch((error) => {});
@@ -52,5 +95,4 @@ const Login: FC<LoginProps> = ({firebase, setLogged }) => {
 	);
 };
 
-
-export default SendingFrame
+export default SendingFrame;
