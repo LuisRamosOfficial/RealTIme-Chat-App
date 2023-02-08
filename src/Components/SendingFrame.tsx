7fdf8411ee737cb45ed706093387a04c54722d10
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2023-02-08 20:59:52 +0000

    Api teste

--- src/Components/SendingFrame.tsx
+++ src/Components/SendingFrame.tsx
@@ -2,7 +2,6 @@ import React, { Dispatch, FC, useState, SetStateAction } from 'react';
 import styles from '../Styles/sendingframe.module.scss';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
-import { LoginAction } from '../assets/Redux/Actions/Actions';
 import send_image from '../assets/send_button.png';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
@@ -11,18 +10,9 @@ interface Props {
 }
 
 const SendingFrame: FC<Props> = ({ firebase }) => {
-	const [logged, setLogged] = useState<boolean>(false);
-
 	return (
 		<div className={styles.sendingframe}>
-			{logged ? (
-				<SendingMessages firebase={firebase} />
-			) : (
-				<>
-					<p>To send messages you need to login 🔰: </p>
-					<Login firebase={firebase} setLogged={setLogged} />
-				</>
-			)}
+			<SendingMessages firebase={firebase} />
 		</div>
 	);
 };
@@ -33,28 +23,36 @@ interface MessageProps {
 
 const SendingMessages: FC<MessageProps> = ({ firebase }) => {
 	const [message, setMessage] = useState<string>('');
+	const [name, setName] = useState<string>('');
 	const db = getFirestore(firebase);
 	const loginInfo = useSelector((state: any) => state.login);
 
 	const UploadHandler = async () => {
-		setMessage('')
+		setMessage('');
 		const today = new Date().toLocaleDateString();
 		const order = Date.now();
 		try {
 			const docRef = await addDoc(collection(db, 'MSG'), {
 				order: order,
-				user: loginInfo.Name,
-				pic: loginInfo.ProfilePic,
-				email: loginInfo.Email,
+				user: name,
+				pic: 'https://divedigital.id/wp-content/uploads/2022/07/2-Blank-PFP-Icon-Instagram.jpg',
+				email: "Michael",
 				message: message,
 				date: today,
 			});
-		} catch (e) {
-		}
+		} catch (e) {}
 	};
 
 	return (
 		<>
+		<span>
+			<h4>Digite o seu Nome:</h4>
+			<input
+				className={styles.Inputname}
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				/>
+				</span>
 			<input
 				className={styles.Input}
 				value={message}
@@ -62,36 +60,6 @@ const SendingMessages: FC<MessageProps> = ({ firebase }) => {
 			/>
 			<img onClick={() => UploadHandler()} src={send_image} />
 		</>
-	);
-};
-
-interface LoginProps {
-	setLogged: Dispatch<SetStateAction<boolean>>;
-	firebase: any;
-}
-const Login: FC<LoginProps> = ({ firebase, setLogged }) => {
-	const dispatch = useDispatch();
-	const auth = getAuth(firebase);
-	const provider = new GoogleAuthProvider();
-
-	const clickHandler = () => {
-		signInWithPopup(auth, provider)
-			.then((result) => {
-				const user = result.user;
-
-				if (user.displayName && user.photoURL && user.email) {
-					dispatch(LoginAction(user.displayName, user.photoURL, user.email));
-					setLogged(true);
-				} else {
-				}
-			})
-			.catch((error) => {});
-	};
-
-	return (
-		<button className={styles.Login} onClick={() => clickHandler()}>
-			Login
-		</button>
 	);
 };
 
